e5841d00504585e5054abd45e3e5bbd7b3459ede
Rakibul islam Mridul <86734518+rakibulmridul@users.noreply.github.com>
2022-10-11 22:29:11 +0600

    Initial commit

---
+++ login.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>Login - Real Estate Management System</title>
+<meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+ 	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
+  <link rel="stylesheet" href="assets/logincss.css"/>
+  <script src="assets/jquery-1.9.1.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.js"></script>
+  <script src="assets/script.js"></script>
+
+
+
+<!-- Owl stylesheet -->
+<link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
+<link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
+<script src="assets/owl-carousel/owl.carousel.js"></script>
+<!-- Owl stylesheet -->
+
+
+<!-- slitslider -->
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css" />
+    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css" />
+    <script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js"></script>
+    <script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js"></script>
+<!-- slitslider -->
+
+<script src='assets/google_analytics_auto.js'></script></head>
+
+
+
+
+<body>
+  <!-- Header Starts -->
+<div class="navbar-wrapper">
+
+<div class="navbar-inverse" role="navigation">
+  <div class="container">
+    <div class="navbar-header">
+
+
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+
+    </div>
+
+
+    <!-- Nav Starts -->
+    <div class="navbar-collapse  collapse">
+      <ul class="nav navbar-nav navbar-right">
+       <li class="active"><a href="index.php">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </div>
+    <!-- #Nav Ends -->
+
+  </div>
+</div>
+
+</div>
+<!-- #Header Starts -->
+<form action="index.php" method="post">
+  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+    <button type="button" class="cancelbtn">Cancel</button>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form> 
+
+<?php 
+
+
+
+if(isset($_POST['username'])){
+    
+    $uname=$_POST['username'];
+    $password=$_POST['password'];
+    
+    $sql="select * from loginform where user='".$uname."'AND Pass='".$password."' limit 1";
+    
+    $result=mysql_query($sql);
+    
+    if(mysql_num_rows($result)==1){
+        echo " You Have Successfully Logged in";
+        exit();
+    }
+    else{
+        echo " You Have Entered Incorrect Password";
+        exit();
+    }
+        
+}
+?>
+
+</div>
+
+</div>
+</div>
+</div>
+
+
+
+
+<div class="footer">
+
+<div class="container">
+
+
+
+<div class="row">
+            <div class="col-lg-3 col-sm-3">
+                   <h4>Information</h4>
+                   <ul class="row">
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="index.php">Home</a></li>
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="about.html">About</a></li>
+                <li class="col-lg-12 col-sm-12 col-xs-3"><a href="contact.html">Contact</a></li>
+              </ul>
+            </div>
+
+            <div class="col-lg-3 col-sm-3">
+                    <h4>Newsletter</h4>
+                    <p>Get notified about the latest properties in our marketplace.</p>
+                    <form class="form-inline" role="form">
+                            <input type="text" placeholder="Enter Your email address" class="form-control">
+                                <button class="btn btn-success" type="button">Notify Me!</button></form>
+            </div>
+
+            <div class="col-lg-3 col-sm-3">
+                    <h4>Follow us</h4>
+                    <a href="#"><img src="images/facebook.png" alt="facebook"></a>
+                    <a href="#"><img src="images/twitter.png" alt="twitter"></a>
+                    <a href="#"><img src="images/linkedin.png" alt="linkedin"></a>
+                    <a href="#"><img src="images/instagram.png" alt="instagram"></a>
+            </div>
+
+            <div class="col-lg-3 col-sm-3">
+              <h4>Contact us</h4>
+              <p><b>InertTech</b><br>
+<span class="glyphicon glyphicon-envelope"></span>www.InertTech.com<br>
+<span class="glyphicon glyphicon-earphone"></span> 01710000000</p>
+      </div>
+  </div>
+<p class="copyright">Copyright 2022. All rights reserved By InertTech.	</p>
+
+
+</div></div>
